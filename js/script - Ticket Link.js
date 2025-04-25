9eebb8f5afdbafa27b1ed341b62c3070171c0f96
Joel I <jitwaru2@hotmail.com>
2025-04-25 00:46:11 -0400

    link to index

--- js/script - Ticket Link.js
+++ js/script - Ticket Link.js
@@ -1,1 +1,4 @@
-window.location.replace("https://rebrand.ly/e1ccaeu");
+// window.location.replace("https://rebrand.ly/e1ccaeu");
+
+// index.html
+window.location.replace("https://rebrand.ly/w5le5nm");
